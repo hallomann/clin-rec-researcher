49bef521362c4026896043f523c3eb43c035462a
dararr-hub <daria.razar@gmail.com>
2025-12-29 14:02:20 +0500

    Теперь лучше обновляет словарик

--- adtodict.py
+++ adtodict.py
@@ -1,12 +1,15 @@
 # -*- codecs: utf-8 -*-
 import codecs
-
+import ast
 
 def adtodict(name):
-    with codecs.open("clindict.py", "r", "utf-8") as file:
-        e = file.readlines()
-    ind = name.find(" ")
-    e.insert(-1, f"          '{name[:ind]}' : '{name}',\n")
-    e = "".join(e)
-    with codecs.open("clindict.py", "w", "utf-8") as file:
+    with codecs.open('clindict.py', 'r', "utf-8") as file:
+        e=file.read()
+    
+    ind=name.find(' ')
+    mydict=ast.literal_eval(e[9:])
+    mydict.update({name[:ind]:name})
+    e=f'thisdict={mydict}'
+
+    with codecs.open('clindict.py','w', "utf-8") as file:
         file.write(e)
